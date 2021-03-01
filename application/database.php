9c675cfe99271c463c0f689d8e4503152c1eef79
刘孙全 <3206883466@qq.com>
2021-03-01 18:09:42 +0800

    open

--- application/database.php
+++ application/database.php
@@ -19,7 +19,7 @@ return [
     // 用户名
     'username'        => 'kaiyuan',
     // 密码
-    'password'        => 'kaiyuans',
+    'password'        => 'kaiyuan',
 
     // 端口
     'hostport'        => '3306',
